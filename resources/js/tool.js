551a33c3af0770b296119efbc5638d670e9115df
Gustavo Gentil <gustavorgentil@outlook.com>
2021-02-05 21:41:15 -0300

    Disable no-undef and global-require

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,28 +1,29 @@
+/* eslint-disable no-undef, global-require */
 import Vuelidate from 'vuelidate';
 import VueTheMask from 'vue-the-mask';
 
-Nova.booting((Vue, router, store) => {
-    Vue.use(Vuelidate);
-    Vue.use(VueTheMask);
+Nova.booting((Vue, router) => {
+  Vue.use(Vuelidate);
+  Vue.use(VueTheMask);
 
-    Vue.filter('dollars', function (value) {
-        if (!value) {
-            return '-';
-        }
+  Vue.filter('dollars', (value) => {
+    if (!value) {
+      return '-';
+    }
 
-        const dollars = value / 100;
+    const dollars = value / 100;
 
-        return dollars.toLocaleString('en-US', {
-            style: 'currency',
-            currency: 'USD',
-        });
+    return dollars.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
     });
+  });
 
-    router.addRoutes([
-        {
-            name: 'booking',
-            path: '/booking',
-            component: require('./components/Tool'),
-        },
-    ]);
+  router.addRoutes([
+    {
+      name: 'booking',
+      path: '/booking',
+      component: require('./components/Tool.vue'),
+    },
+  ]);
 });
