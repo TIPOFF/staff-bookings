33e6ea9eec88d8cd77ae07667825fcd595a51909
Drew Roberts <github@drewroberts.com>
2021-02-13 10:31:48 -0500

    Remove policies since not created yet

--- src/StaffBookingsServiceProvider.php
+++ src/StaffBookingsServiceProvider.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Tipoff\StaffBookings;
 
-use Tipoff\StaffBookings\Models\StaffBooking;
-use Tipoff\StaffBookings\Policies\StaffBookingPolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -14,9 +12,6 @@ class ToolServiceProvider extends TipoffServiceProvider
     public function configureTipoffPackage(TipoffPackage $package): void
     {
         $package
-            ->hasPolicies([
-                StaffBookings::class => StaffBookingPolicy::class,
-            ])
             ->name('staff-bookings')
             ->hasConfigFile();
     }
